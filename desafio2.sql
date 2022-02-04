bda3f323acb5c3ba21ae7a9e9ad3cc3ad4754984
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2022-02-04 15:25:17 -0300

    🚀 Requisito 02

---
+++ desafio2.sql
@@ -0,0 +1,4 @@
+SELECT
+	(SELECT COUNT(cancao_id) FROM SpotifyClone.cancoes) AS cancoes,
+    (SELECT COUNT(artista_id) FROM SpotifyClone.artistas) AS artistas,
+    (SELECT COUNT(album_id) FROM SpotifyClone.albuns) AS albuns;
